86fa440feb4dd4d5312287b55402cf515f53bed0
Paul Heggeseth <pheggeseth@launchdarkly.com>
2022-02-20 14:02:58 -0600

    add types

--- src/components/Calendar/utils.ts
+++ src/components/Calendar/utils.ts
@@ -5,10 +5,10 @@ export function getDaysInVisibleMonth(visibleMonth: number, visibleYear: number)
   const endDate = dayjs([visibleYear, visibleMonth]).endOf('month').weekday(6);
   const dayCount = endDate.diff(startDate, 'days') + 1;
 
-  const weeks = [];
+  const weeks: string[][] = [];
 
   for (let i = 0; i < dayCount / 7; i++) {
-    const week = [];
+    const week: string[] = [];
     for (let j = 0; j < 7; j++) {
       week.push(startDate.add(i * 7 + j, 'days').format('YYYY-MM-DD'));
     }
