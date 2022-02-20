fceb152dbc1cde9e7523375e1ef9e879b3ab94ea
Paul Heggeseth <pheggeseth@launchdarkly.com>
2022-02-20 13:16:49 -0600

    add Popover demo

---
+++ src/demo/Popover.tsx
@@ -0,0 +1,31 @@
+import Popover, { PopoverContextMemo } from '~/components/Popover';
+
+export default function PopoverDemo() {
+  let outerContext: PopoverContextMemo;
+  let innerContext: PopoverContextMemo;
+
+  return (
+    <section>
+      <h2>Popover</h2>
+      <Popover context={(ctx) => (outerContext = ctx)}>
+        <Popover.Trigger>{outerContext().isOpen ? 'Close' : 'Open'}</Popover.Trigger>
+        <Popover.Overlay class="popover-overlay" />
+        <Popover.Panel class="popover">
+          <Popover.Trigger>X</Popover.Trigger>
+          <button>1</button>
+          <Popover context={(ctx) => (innerContext = ctx)}>
+            <button ref={Popover.AnchorRef}>2</button>
+            <button>3</button>
+            <Popover.Trigger>{innerContext().isOpen ? 'Close' : 'Open'}</Popover.Trigger>
+            <Popover.Overlay class="popover-overlay" />
+            <Popover.Panel class="popover">
+              <button>1</button>
+              <button>2</button>
+              <button>3</button>
+            </Popover.Panel>
+          </Popover>
+        </Popover.Panel>
+      </Popover>
+    </section>
+  );
+}
