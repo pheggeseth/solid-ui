f01515896eaa6b0ac53462fe9bb368baccf45af2
Paul Heggeseth <pheggeseth@launchdarkly.com>
2022-03-02 13:26:08 -0600

    add composable label and list logic

---
+++ src/components2/List.tsx
@@ -0,0 +1,332 @@
+import {
+  Accessor,
+  createContext,
+  createEffect,
+  createMemo,
+  createSelector,
+  JSX,
+  mergeProps,
+  onCleanup,
+  onMount,
+  PropsWithChildren,
+  useContext,
+} from 'solid-js';
+import { createStore, DeepReadonly } from 'solid-js/store';
+import { ListboxOrientation } from '~/types';
+import { useId } from '~/utils/componentUtils';
+import { useKeyEventHandlers } from '~/utils/eventUtils';
+import { LabelProvider, useLabelState } from './Label';
+
+type ListItem<ItemValue> = {
+  id: string;
+  value?: ItemValue;
+};
+
+type ListElementIds = {
+  labelId: string;
+};
+
+type ListState<ItemValue> = ListElementIds & {
+  activeItemId: string;
+  items: ListItem<ItemValue>[];
+  orientation: ListboxOrientation;
+};
+
+type ListSelectors = {
+  isItemActive: (itemId: string) => boolean;
+};
+
+type ListActions<ItemValue> = {
+  setElementId(name: keyof ListElementIds, id: string): void;
+  addItem(item: ListItem<ItemValue>): void;
+  removeItem(itemId: string): void;
+  initializeItemFocus(): void;
+  focusNextItem(): void;
+  focusPreviousItem(): void;
+  focusFirstItem(): void;
+  focusLastItem(): void;
+  focusItemStartingWith(search: string): void;
+  hoverItem(itemId: string): void;
+  hoverItemClear(): void;
+  chooseFocusedItem(): void;
+  chooseItem(itemId: string): void;
+};
+
+const ListContext = createContext<{
+  state: ListState<any>;
+  selectors: ListSelectors;
+  actions: ListActions<any>;
+}>();
+
+function useListState<ItemValue>() {
+  return useContext(ListContext).state as ListState<ItemValue>;
+}
+
+function useListSelectors() {
+  return useContext(ListContext).selectors;
+}
+
+function useListActions<ItemValue>() {
+  return useContext(ListContext).actions as ListActions<ItemValue>;
+}
+
+export type ListProviderProps<ItemValue> = PropsWithChildren<{
+  'aria-orientation'?: ListboxOrientation;
+  onChange?: (newValue: ItemValue) => void;
+  value?: ItemValue;
+}>;
+
+export function ListProvider<ItemValue>(props: ListProviderProps<ItemValue>) {
+  props = mergeProps<typeof props[]>({ 'aria-orientation': 'vertical' }, props);
+
+  const [state, setState] = createStore<ListState<ItemValue>>({
+    labelId: null,
+    activeItemId: null,
+    items: [],
+    get orientation(): ListboxOrientation {
+      return props['aria-orientation'];
+    },
+  });
+
+  const isActiveItem = (item: ListItem<ItemValue>) => item.id === state.activeItemId;
+  const getActiveItemIndex = () => state.items.findIndex(isActiveItem);
+
+  const selectors: ListSelectors = {
+    isItemActive: createSelector(() => state.activeItemId),
+  };
+
+  const focusItem = (item: DeepReadonly<ListItem<ItemValue>>) => {
+    setState({ activeItemId: item.id });
+  };
+
+  const hasId = (itemId: string) => (item: { id: string }) => item.id === itemId;
+
+  const actions: ListActions<ItemValue> = {
+    setElementId(name, id) {
+      setState(name, id);
+    },
+    addItem(item) {
+      setState('items', (items) => [...items, item]);
+    },
+    removeItem(itemId) {
+      setState('items', (items) => items.filter((item) => !hasId(itemId)(item)));
+    },
+    initializeItemFocus() {
+      if (state.activeItemId) {
+        return;
+      }
+
+      const itemToFocus = state.items.find((item) => item.value === props.value) || state.items[0];
+
+      if (itemToFocus) {
+        focusItem(itemToFocus);
+      }
+    },
+    focusNextItem() {
+      const activeItemIndex = getActiveItemIndex();
+      if (activeItemIndex < state.items.length - 1) {
+        focusItem(state.items[activeItemIndex + 1]);
+      }
+    },
+    focusPreviousItem() {
+      const activeItemIndex = getActiveItemIndex();
+
+      if (activeItemIndex === -1) {
+        focusItem(state.items[state.items.length - 1]);
+      } else if (activeItemIndex > 0) {
+        focusItem(state.items[activeItemIndex - 1]);
+      }
+    },
+    focusFirstItem() {
+      focusItem(state.items[0]);
+    },
+    focusLastItem() {
+      focusItem(state.items[state.items.length - 1]);
+    },
+    focusItemStartingWith(search: string) {
+      const activeItemIndex = getActiveItemIndex();
+      const wrappedItems = state.items
+        .slice(Math.min(activeItemIndex + 1, state.items.length - 1))
+        .concat(state.items.slice(0, activeItemIndex));
+
+      const searchTerm = search.toLocaleLowerCase();
+      const matchingItem = wrappedItems.find((item) =>
+        document.getElementById(item.id).textContent.toLocaleLowerCase().startsWith(searchTerm)
+      );
+
+      if (matchingItem) {
+        focusItem(matchingItem);
+      }
+    },
+    hoverItem(itemId) {
+      setState({
+        activeItemId: state.items.find(hasId(itemId))?.id || null,
+      });
+    },
+    hoverItemClear() {
+      setState({ activeItemId: null });
+    },
+    chooseFocusedItem() {
+      const activeItem = state.items.find(isActiveItem);
+      if (activeItem) {
+        props.onChange?.(activeItem.value as ItemValue);
+      }
+    },
+    chooseItem(itemId) {
+      const item = state.items.find(hasId(itemId));
+      if (item) {
+        props.onChange?.(item.value as ItemValue);
+      }
+    },
+  };
+
+  return (
+    <LabelProvider>
+      <ListContext.Provider value={{ state: state as ListState<any>, selectors, actions }}>
+        {props.children}
+      </ListContext.Provider>
+    </LabelProvider>
+  );
+}
+
+export type ListProps<ListElement extends HTMLElement> = {
+  'aria-activedescendent': string;
+  'aria-labelledby': string;
+  id: string;
+  onFocus: JSX.EventHandler<ListElement, FocusEvent>;
+  onKeyDown: JSX.EventHandler<ListElement, KeyboardEvent>;
+  tabIndex: 0;
+};
+
+export function createListProps<ItemValue, ListContainerElement extends HTMLElement>(config: {
+  idPrefix: string;
+}): ListProps<ListContainerElement> {
+  const labelState = useLabelState();
+  const listState = useListState<ItemValue>();
+  const listActions = useListActions<ItemValue>();
+
+  const id = useId(config.idPrefix);
+
+  let search: string;
+  let resetSearch: NodeJS.Timeout;
+
+  return {
+    get ['aria-activedescendent']() {
+      return listState.activeItemId;
+    },
+    get ['aria-labelledby']() {
+      return labelState.labelId;
+    },
+    id,
+    onFocus() {
+      listActions.initializeItemFocus();
+    },
+    onKeyDown: useKeyEventHandlers<ListContainerElement>({
+      ArrowUp(event) {
+        if (listState.orientation === 'vertical') {
+          event.preventDefault();
+          listActions.focusPreviousItem();
+        }
+      },
+      ArrowDown(event) {
+        if (listState.orientation === 'vertical') {
+          event.preventDefault();
+          listActions.focusNextItem();
+        }
+      },
+      ArrowLeft(event) {
+        if (listState.orientation === 'horizontal') {
+          event.preventDefault();
+          listActions.focusPreviousItem();
+        }
+      },
+      ArrowRight(event) {
+        if (listState.orientation === 'horizontal') {
+          event.preventDefault();
+          listActions.focusNextItem();
+        }
+      },
+      Home(event) {
+        event.preventDefault();
+        listActions.focusFirstItem();
+      },
+      End(event) {
+        event.preventDefault();
+        listActions.focusLastItem();
+      },
+      Enter(event) {
+        event.preventDefault();
+        listActions.chooseFocusedItem();
+      },
+      [' '](event) {
+        if (search) {
+          this.default(event);
+        } else {
+          event.preventDefault();
+          listActions.chooseFocusedItem();
+        }
+      },
+      default(event) {
+        if (event.key.length === 1) {
+          event.preventDefault();
+          clearTimeout(resetSearch);
+          search += event.key;
+          listActions.focusItemStartingWith(search);
+
+          resetSearch = setTimeout(() => {
+            search = '';
+          }, 500);
+        }
+      },
+    }),
+    tabIndex: 0,
+  };
+}
+
+export type ListItemProps<ListItemElement extends HTMLElement> = {
+  'aria-selected': boolean;
+  'data-active': string;
+  id: string;
+  onClick: JSX.EventHandler<ListItemElement, MouseEvent>;
+  onMouseEnter: JSX.EventHandler<ListItemElement, MouseEvent>;
+  onMouseLeave: JSX.EventHandler<ListItemElement, MouseEvent>;
+};
+
+export function createListItemProps<ItemValue, ListItemElement extends HTMLElement>(confg: {
+  idPrefix: string;
+  value?: ItemValue;
+}): ListItemProps<ListItemElement> {
+  const listState = useListState<ItemValue>();
+  const listSelectors = useListSelectors();
+  const listActions = useListActions<ItemValue>();
+
+  const id = useId(confg.idPrefix);
+
+  onMount(() => listActions.addItem({ id, value: confg.value }));
+  onCleanup(() => listActions.removeItem(id));
+
+  createEffect(() => {
+    if (listState.activeItemId === id) {
+      document.getElementById(id).scrollIntoView(false);
+    }
+  });
+
+  return {
+    get ['aria-selected']() {
+      return listSelectors.isItemActive(id) || undefined;
+    },
+    get ['data-active']() {
+      return listSelectors.isItemActive(id) ? '' : undefined;
+    },
+    id,
+    onClick() {
+      listActions.chooseItem(id);
+    },
+    onMouseEnter() {
+      listActions.hoverItem(id);
+    },
+    onMouseLeave() {
+      listActions.hoverItemClear();
+    },
+  };
+}
--- src/components2/Listbox3.tsx
+++ src/components2/Listbox3.tsx
@@ -1,238 +1,51 @@
-import {
-  Accessor,
-  createContext,
-  createEffect,
-  createMemo,
-  createSelector,
-  JSX,
-  mergeProps,
-  onCleanup,
-  onMount,
-  PropsWithChildren,
-  splitProps,
-  useContext,
-} from 'solid-js';
-import { createStore, DeepReadonly } from 'solid-js/store';
+import { mergeProps, splitProps } from 'solid-js';
 import { Dynamic } from 'solid-js/web';
-import { BaseComponentProps, DynamicComponent, ListboxOrientation } from '~/types';
-import { useId } from '~/utils/componentUtils';
-import { useKeyEventHandlers } from '~/utils/eventUtils';
+import { BaseComponentProps, DynamicComponent } from '~/types';
+import { createLabelProps } from './Label';
+import {
+  createListItemProps,
+  createListProps,
+  ListItemProps,
+  ListProps,
+  ListProvider,
+  ListProviderProps,
+} from './List';
 
-type ListboxItemType<ItemValue> = {
-  id: string;
-  value?: ItemValue;
-};
-
-type ListboxElementIds = {
-  labelId: string;
-};
-
-type ListboxState<ItemValue> = ListboxElementIds & {
-  activeItemId: string;
-  items: ListboxItemType<ItemValue>[];
-};
-
-type ListboxSelectors = {
-  isItemActive: (itemId: string) => boolean;
-  orientation: Accessor<ListboxOrientation>;
-};
-
-type ListboxActions<ItemValue> = {
-  setElementId(name: keyof ListboxElementIds, id: string): void;
-  addItem(item: ListboxItemType<ItemValue>): void;
-  removeItem(itemId: string): void;
-  initializeItemFocus(): void;
-  focusNextItem(): void;
-  focusPreviousItem(): void;
-  focusFirstItem(): void;
-  focusLastItem(): void;
-  focusItemStartingWith(search: string): void;
-  hoverItem(itemId: string): void;
-  hoverItemClear(): void;
-  chooseFocusedItem(): void;
-  chooseItem(itemId: string): void;
-};
-
-const ListboxContext = createContext<{
-  state: ListboxState<any>;
-  selectors: ListboxSelectors;
-  actions: ListboxActions<any>;
-}>();
-
-function useListboxState<ItemValue>() {
-  return useContext(ListboxContext).state as ListboxState<ItemValue>;
-}
-
-function useListboxSelectors<ItemValue>() {
-  return useContext(ListboxContext).selectors as ListboxSelectors;
-}
-
-function useListboxActions<ItemValue>() {
-  return useContext(ListboxContext).actions as ListboxActions<ItemValue>;
-}
-
-type ListboxProviderProps<ItemValue> = PropsWithChildren<{
-  'aria-orientation'?: ListboxOrientation;
-  onChange?: (newValue: ItemValue) => void;
-  value?: ItemValue;
-}>;
+export type ListboxProviderProps<ItemValue> = ListProviderProps<ItemValue>;
 
 export function ListboxProvider<ItemValue = any>(props: ListboxProviderProps<ItemValue>) {
-  props = mergeProps<typeof props[]>({ 'aria-orientation': 'vertical' }, props);
-
-  const [listboxState, setListboxState] = createStore<ListboxState<ItemValue>>({
-    labelId: null,
-    activeItemId: null,
-    items: [],
-  });
-
-  const isActiveItem = (item: ListboxItemType<ItemValue>) => item.id === listboxState.activeItemId;
-  const getActiveItemIndex = () => listboxState.items.findIndex(isActiveItem);
-
-  const listboxSelectors: ListboxSelectors = {
-    isItemActive: createSelector(() => listboxState.activeItemId),
-    orientation: createMemo(() => props['aria-orientation']),
-  };
-
-  const focusItem = (item: DeepReadonly<ListboxItemType<ItemValue>>) => {
-    setListboxState({ activeItemId: item.id });
-  };
-
-  const hasId = (itemId: string) => (item: { id: string }) => item.id === itemId;
-
-  const listboxActions: ListboxActions<ItemValue> = {
-    setElementId(name, id) {
-      setListboxState(name, id);
-    },
-    addItem(item) {
-      setListboxState('items', (items) => [...items, item]);
-    },
-    removeItem(itemId) {
-      setListboxState('items', (items) => items.filter((item) => !hasId(itemId)(item)));
-    },
-    initializeItemFocus() {
-      if (listboxState.activeItemId) {
-        return;
-      }
-
-      const itemToFocus =
-        listboxState.items.find((item) => item.value === props.value) || listboxState.items[0];
-
-      if (itemToFocus) {
-        focusItem(itemToFocus);
-      }
-    },
-    focusNextItem() {
-      const activeItemIndex = getActiveItemIndex();
-      if (activeItemIndex < listboxState.items.length - 1) {
-        focusItem(listboxState.items[activeItemIndex + 1]);
-      }
-    },
-    focusPreviousItem() {
-      const activeItemIndex = getActiveItemIndex();
-
-      if (activeItemIndex === -1) {
-        focusItem(listboxState.items[listboxState.items.length - 1]);
-      } else if (activeItemIndex > 0) {
-        focusItem(listboxState.items[activeItemIndex - 1]);
-      }
-    },
-    focusFirstItem() {
-      focusItem(listboxState.items[0]);
-    },
-    focusLastItem() {
-      focusItem(listboxState.items[listboxState.items.length - 1]);
-    },
-    focusItemStartingWith(search: string) {
-      const activeItemIndex = getActiveItemIndex();
-      const wrappedItems = listboxState.items
-        .slice(Math.min(activeItemIndex + 1, listboxState.items.length - 1))
-        .concat(listboxState.items.slice(0, activeItemIndex));
-
-      const searchTerm = search.toLocaleLowerCase();
-      const matchingItem = wrappedItems.find((item) =>
-        document.getElementById(item.id).textContent.toLocaleLowerCase().startsWith(searchTerm)
-      );
-
-      if (matchingItem) {
-        focusItem(matchingItem);
-      }
-    },
-    hoverItem(itemId) {
-      setListboxState({
-        activeItemId: listboxState.items.find(hasId(itemId))?.id || null,
-      });
-    },
-    hoverItemClear() {
-      setListboxState({ activeItemId: null });
-    },
-    chooseFocusedItem() {
-      const activeItem = listboxState.items.find(isActiveItem);
-      if (activeItem) {
-        props.onChange?.(activeItem.value as ItemValue);
-      }
-    },
-    chooseItem(itemId) {
-      const item = listboxState.items.find(hasId(itemId));
-      if (item) {
-        props.onChange?.(item.value as ItemValue);
-      }
-    },
-  };
-
-  return (
-    <ListboxContext.Provider
-      value={{
-        state: listboxState as ListboxState<any>,
-        selectors: listboxSelectors,
-        actions: listboxActions,
-      }}
-    >
-      {props.children}
-    </ListboxContext.Provider>
-  );
+  return <ListProvider {...props}>{props.children}</ListProvider>;
 }
 
-type ListboxLabelProps<ListboxLabelElement extends HTMLElement> = BaseComponentProps<{
+type ListboxLabelProps = BaseComponentProps<{
   component?: DynamicComponent<{
     id: string;
   }>;
   idPrefix?: string;
 }>;
 
-export function ListboxLabel<ListboxLabelElement extends HTMLElement = HTMLSpanElement>(
-  props: ListboxLabelProps<ListboxLabelElement>
-) {
+export function ListboxLabel(props: ListboxLabelProps) {
   props = mergeProps<typeof props[]>(
     { component: 'span', idPrefix: 'solid-ui-listbox-label' },
     props
   );
 
-  const listboxActions = useListboxActions();
+  const [localProps, otherProps] = splitProps(props, ['component', 'idPrefix']);
 
-  const id = useId(props.idPrefix);
-
-  onMount(() => {
-    listboxActions.setElementId('labelId', id);
-  });
-
-  const [localProps, otherProps] = splitProps(props, ['component']);
+  const labelProps = createLabelProps({ idPrefix: localProps.idPrefix });
 
   return (
-    <Dynamic {...otherProps} component={localProps.component} data-solid-ui-listbox-label id={id} />
+    <Dynamic
+      {...otherProps}
+      {...labelProps}
+      component={localProps.component}
+      data-solid-ui-listbox-label=""
+    />
   );
 }
 
 type ListboxProps<ListboxElement extends HTMLElement> = BaseComponentProps<{
-  component?: DynamicComponent<{
-    'aria-activedescendent': string;
-    'aria-labelledby': string;
-    id: string;
-    onFocus: JSX.EventHandler<ListboxElement, FocusEvent>;
-    onKeyDown: JSX.EventHandler<ListboxElement, KeyboardEvent>;
-    role: string;
-    tabIndex: string | number;
-  }>;
+  component?: DynamicComponent<ListProps<ListboxElement> & { role: string }>;
   idPrefix?: string;
   role?: string;
 }>;
@@ -245,88 +58,17 @@ export function Listbox<ItemValue, ListboxElement extends HTMLElement = HTMLULis
     props
   );
 
-  const id = useId(props.idPrefix);
-
-  const listboxState = useListboxState<ItemValue>();
-  const listboxSelectors = useListboxSelectors<ItemValue>();
-  const listboxActions = useListboxActions<ItemValue>();
-
   const [localProps, otherProps] = splitProps(props, ['component', 'idPrefix', 'role']);
 
-  let search = '';
-  let resetSearch: NodeJS.Timeout;
+  const listProps = createListProps<ItemValue, ListboxElement>({ idPrefix: localProps.idPrefix });
 
   return (
     <Dynamic
       {...otherProps}
-      aria-activedescendent={listboxState.activeItemId}
-      aria-labelledby={listboxState.labelId}
-      data-solid-ui-listbox=""
+      {...listProps}
       component={localProps.component}
-      id={id}
-      onFocus={() => {
-        listboxActions.initializeItemFocus();
-      }}
-      onKeyDown={useKeyEventHandlers<ListboxElement>({
-        ArrowUp(event) {
-          if (listboxSelectors.orientation() === 'vertical') {
-            event.preventDefault();
-            listboxActions.focusPreviousItem();
-          }
-        },
-        ArrowDown(event) {
-          if (listboxSelectors.orientation() === 'vertical') {
-            event.preventDefault();
-            listboxActions.focusNextItem();
-          }
-        },
-        ArrowLeft(event) {
-          if (listboxSelectors.orientation() === 'horizontal') {
-            event.preventDefault();
-            listboxActions.focusPreviousItem();
-          }
-        },
-        ArrowRight(event) {
-          if (listboxSelectors.orientation() === 'horizontal') {
-            event.preventDefault();
-            listboxActions.focusNextItem();
-          }
-        },
-        Home(event) {
-          event.preventDefault();
-          listboxActions.focusFirstItem();
-        },
-        End(event) {
-          event.preventDefault();
-          listboxActions.focusLastItem();
-        },
-        Enter(event) {
-          event.preventDefault();
-          listboxActions.chooseFocusedItem();
-        },
-        [' '](event) {
-          if (search) {
-            this.default(event);
-          } else {
-            event.preventDefault();
-            listboxActions.chooseFocusedItem();
-          }
-        },
-        default(event) {
-          if (event.key.length === 1) {
-            event.preventDefault();
-            clearTimeout(resetSearch);
-            search += event.key;
-            listboxActions.focusItemStartingWith(search);
-
-            resetSearch = setTimeout(() => {
-              search = '';
-            }, 500);
-          }
-        },
-      })}
+      data-solid-ui-listbox=""
       role={localProps.role}
-      tabIndex={0}
     />
   );
 }
@@ -335,13 +77,7 @@ export type ListboxItemProps<
   ItemValue,
   ListboxItemElement extends HTMLElement
 > = BaseComponentProps<{
-  component?: DynamicComponent<{
-    id: string;
-    onClick: JSX.EventHandler<ListboxItemElement, MouseEvent>;
-    onMouseEnter: JSX.EventHandler<ListboxItemElement, MouseEvent>;
-    onMouseLeave: JSX.EventHandler<ListboxItemElement, MouseEvent>;
-    role: string;
-  }>;
+  component?: DynamicComponent<ListItemProps<ListboxItemElement> & { role: string }>;
   idPrefix?: string;
   role?: string;
   value?: ItemValue;
@@ -355,40 +91,19 @@ export function ListboxItem<ItemValue, ListboxItemElement extends HTMLElement = 
     props
   );
 
-  const listboxState = useListboxState<ItemValue>();
-  const listboxSelectors = useListboxSelectors<ItemValue>();
-  const listboxActions = useListboxActions<ItemValue>();
+  const [localProps, otherProps] = splitProps(props, ['component', 'idPrefix', 'role', 'value']);
 
-  const id = useId(props.idPrefix);
-
-  onMount(() => listboxActions.addItem({ id, value: props.value }));
-  onCleanup(() => listboxActions.removeItem(id));
-
-  createEffect(() => {
-    if (listboxState.activeItemId === id) {
-      document.getElementById(id).scrollIntoView(false);
-    }
+  const listItemProps = createListItemProps({
+    idPrefix: localProps.idPrefix,
+    value: localProps.value,
   });
-
-  const [localProps, otherProps] = splitProps(props, ['component', 'idPrefix', 'role']);
 
   return (
     <Dynamic
       {...otherProps}
-      aria-selected={listboxSelectors.isItemActive(id) || undefined}
-      data-active={listboxSelectors.isItemActive(id) ? '' : undefined}
+      {...listItemProps}
       data-solid-ui-listbox-item=""
       component={localProps.component}
-      id={id}
-      onClick={() => {
-        listboxActions.chooseItem(id);
-      }}
-      onMouseEnter={() => {
-        listboxActions.hoverItem(id);
-      }}
-      onMouseLeave={() => {
-        listboxActions.hoverItemClear();
-      }}
       role={localProps.role}
     />
   );
