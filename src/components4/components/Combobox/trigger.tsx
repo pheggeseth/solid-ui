34205c930dd6cad22fc5e43d28e7bc4e1fb12410
Paul Heggeseth <pheggeseth@launchdarkly.com>
2022-03-29 21:13:53 -0500

    remove standard data attribute from combobox trigger

--- src/components4/components/Combobox/trigger.tsx
+++ src/components4/components/Combobox/trigger.tsx
@@ -1,4 +1,4 @@
-import { createEffect, JSX, mergeProps, onMount } from 'solid-js';
+import { JSX, mergeProps, onMount } from 'solid-js';
 import { getDataProp, useId } from '~/utils/componentUtils';
 import { useComboboxActions, useComboboxContext, useComboboxState } from './context';
 
@@ -39,7 +39,6 @@ export function createTriggerProps<TriggerElement extends HTMLElement>(
     get ['aria-labelledby']() {
       return state.labelId;
     },
-    'data-solid-ui-button': '',
     ...getDataProp(idPrefix),
     id,
     tabIndex: -1,
