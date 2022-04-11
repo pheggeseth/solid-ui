0858ece946a9971fcd5b8ca77bb6d48de0375726
Paul Heggeseth <pheggeseth@launchdarkly.com>
2022-04-11 13:48:57 -0500

    add mobile nav

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -1,3 +1,4 @@
+import { createSignal } from 'solid-js';
 import '../styles.css';
 import { Background } from './Background';
 import { CalendarDemo } from './components/Calendar';
@@ -12,18 +13,68 @@ import { SwitchDemo } from './components/Switch';
 import './demo.css';
 import { Extensions } from './Extensions';
 import { Styling } from './Styling';
-import { Code } from './utils';
 
-export function Demo() {
+const MenuOpen = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 0 24 24"
+    width="24px"
+    fill="#FFFFFF"
+  >
+    <path d="M0 0h24v24H0V0z" fill="none" />
+    <path d="M3 18h13v-2H3v2zm0-5h10v-2H3v2zm0-7v2h13V6H3zm18 9.59L17.42 12 21 8.41 19.59 7l-5 5 5 5L21 15.59z" />
+  </svg>
+);
+
+const MenuClose = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 0 24 24"
+    width="24px"
+    fill="#FFFFFF"
+  >
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+  </svg>
+);
+
+function Nav() {
+  const [isOpen, setIsOpen] = createSignal(false);
   return (
     <>
-      <nav>
+      <button
+        id="open-menu"
+        title="Open menu"
+        classList={{ open: isOpen() }}
+        onClick={() => setIsOpen(true)}
+      >
+        <MenuOpen />
+      </button>
+      <div id="nav-overlay" classList={{ open: isOpen() }} onClick={() => setIsOpen(false)} />
+      <nav
+        classList={{ open: isOpen() }}
+        onClick={(event) => {
+          if (event.target.matches('a')) {
+            setIsOpen(false);
+          }
+        }}
+      >
         <header>
           <h1>
             <a href="#Home">
               SOLID<strong>UI</strong>
             </a>
           </h1>
+          <button
+            id="close-menu"
+            title="Close menu"
+            classList={{ open: isOpen() }}
+            onClick={() => setIsOpen(false)}
+          >
+            <MenuClose />
+          </button>
         </header>
         <ul>
           <li>
@@ -67,11 +118,21 @@ export function Demo() {
           </li>
         </ul>
       </nav>
+    </>
+  );
+}
+
+export function Demo() {
+  return (
+    <>
+      <Nav />
       <main>
-        <h1 id="Home">
-          SOLID<strong>UI</strong>
-        </h1>
-        <p>Hooks for building declarative, accessible, composable UI components with SolidJS.</p>
+        <header>
+          <h1 id="Home">
+            SOLID<strong>UI</strong>
+          </h1>
+          <p>Hooks for building declarative, accessible, composable UI components with SolidJS.</p>
+        </header>
         <Background />
         <Styling />
         <Extensions />
